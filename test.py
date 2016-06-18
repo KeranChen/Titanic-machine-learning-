79fd28ddace60af2efa1aef74855e82e89d1d90c
Keran <Keran@KeranmatoMacBook-Air.local>
2016-06-19 04:40:44 +0800

    Modified test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print 'heihei'
+"""
+1. create a directory
+2. go to directory and enter git init
+3. create some file like test.py
+4. git status  to see if there is anychange
+5. git add test.py
+6. git commit -m "Added a new file"
+7. git push
+8. done
+"""
